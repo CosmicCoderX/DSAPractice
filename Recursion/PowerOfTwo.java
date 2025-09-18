5d45736958ed862acee632487d56d4dc0c1c3391
Suryakant Dwivedi <suryakantdwivedi8493@gmail.coms>
2025-09-18 23:43:09 +0530

    New Codes are being uploaded

--- Recursion/PowerOfTwo.java
+++ Recursion/PowerOfTwo.java
@@ -6,14 +6,14 @@ public class PowerOfTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Power(n);
+        System.out.println(Power(n));
     }
     static boolean Power(int n){
         if(n == 1){
             return true;
         }
         if(n<=0 || n%2 !=0) return false;
-        Power(n/2);
+        return Power(n/2);
 
     }
 }
