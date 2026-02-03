6e9c0251467db1b4584ce5c211bf6a5827df5170
Suryakant Dwivedi <suryakantdwivedi8493@gmail.coms>
2026-02-04 03:30:06 +0530

    New Codes are being uploaded

---
+++ Stack/BasicsOfStack.java
@@ -0,0 +1,59 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BasicsOfStack {
+    public static void main(String[] args) {
+        // ArrayList<Integer> arr = new ArrayList<>();
+        //Stack Initialization->
+        Stack<Integer> st = new Stack<>();
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+//        PUSH->
+        st.push(1);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+        st.push(30);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+        st.push(56);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+        st.push(90);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+        st.push(5);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+        st.push(24);
+        System.out.println("Size is: "+st.size());
+        System.out.println(st);
+
+//        PEEK->
+        System.out.println(st.peek());
+
+//        POP->
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+
+        System.out.println("Size is: "+st.size());
+
+//        Iterate to first element->
+        while(st.size() > 1){
+            st.pop();
+        }
+        System.out.println(st.peek());
+        System.out.println(st);
+    }
+}
